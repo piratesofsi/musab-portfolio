0f07853329b9736b8f65a6c4560a44b8deff8d40
Musab Shaikh <146651306+piratesofsi@users.noreply.github.com>
2026-01-31 18:57:19 +0530

    update

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
   return (
     <section
       id="home"
-      className="min-h-screen md:max-w-[1600px] md:mx-auto -mt-24 md:mt-7 flex flex-col-reverse md:flex-row items-center gap-14 justify-start px-10 md:px-20 
+      className="min-h-screen md:max-w-[1600px] md:mx-auto  md:mt-7 flex flex-col-reverse md:flex-row items-center gap-14 justify-start px-10 md:px-20 
 "
     >
       {/* LEFT TEXT */}
